bdde3de709fe200d47c488d9ab79a3be1957507a
Win10 <Win10@DESKTOP-UT5FVV3>
2024-02-05 17:10:21 +0530

    Add project files.

---
+++ WebAppwithData/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppwithData.Models;
+using WebAppwithData.Services;
+
+namespace WebAppwithData.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<Product> products;
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ProductService prodService = new ProductService();
+            products = prodService.GetProducts();
+        }
+    }
+}
---
+++ WebAppwithData/Services/ProductService.cs
@@ -0,0 +1,71 @@
+﻿using System.Data.Common;
+using System.Data;
+using System.Data.SqlClient;
+using WebAppwithData.Models;
+
+namespace WebAppwithData.Services
+{
+    public class ProductService
+    {
+
+        private static string db_source = "appdbserver1291.database.windows.net";
+        private static string db_username = "sqladmin";
+        private static string db_password = "Password@123";
+        private static string db_database = "webappdb";
+
+        private SqlConnection GetConnection()
+        {
+            SqlConnectionStringBuilder sqlConnStringBuilder = new SqlConnectionStringBuilder();
+            sqlConnStringBuilder.DataSource = db_source;
+            sqlConnStringBuilder.UserID = db_username;
+            sqlConnStringBuilder.Password = db_password;
+            sqlConnStringBuilder.InitialCatalog = db_database;
+            return new SqlConnection(sqlConnStringBuilder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            
+            var products = new List<Product>();
+            SqlConnection conn = GetConnection();
+            string sqlQuery = "SELECT ProductID, ProductName, Quantity from Products";
+            try
+            {
+
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+                using (SqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        var prod = new Product()
+                        {
+                            ProductID = sdr.GetInt32(0),
+                            ProductName = sdr.GetString(1),
+                            Quantity = sdr.GetInt32(2)
+                        };
+
+                        products.Add(prod);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn != null && conn.State == ConnectionState.Open)
+                {
+                    conn.CloseAsync();
+                }
+            }
+
+
+            return products;
+
+        }
+	}
+}
